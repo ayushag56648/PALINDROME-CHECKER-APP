036a24de86cf10b080f83b312c2db812dca0681b
ayushag56648 <ayush.ag56648@gmail.com>
2026-03-11 10:52:09 +0530

    UC9: Recursive palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,73 +1,29 @@
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
+    // Recursive function
+    public static boolean isPalindrome(String word, int start, int end) {
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public static boolean isPalindrome(String word) {
-
-        Node head = null;
-        Node tail = null;
-
-        // Convert string to linked list
-        for(char ch : word.toCharArray()) {
-            Node newNode = new Node(ch);
-
-            if(head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
+        // Base condition
+        if(start >= end) {
+            return true;
         }
 
-        // Find middle using fast and slow pointers
-        Node slow = head;
-        Node fast = head;
-
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        // Check first and last characters
+        if(word.charAt(start) != word.charAt(end)) {
+            return false;
         }
 
-        // Reverse second half
-        Node prev = null;
-        Node current = slow;
-
-        while(current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        // Compare halves
-        Node first = head;
-        Node second = prev;
-
-        while(second != null) {
-            if(first.data != second.data) {
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }
-
-        return true;
+        // Recursive call
+        return isPalindrome(word, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        if(isPalindrome(word)) {
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+
+        if(result) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
